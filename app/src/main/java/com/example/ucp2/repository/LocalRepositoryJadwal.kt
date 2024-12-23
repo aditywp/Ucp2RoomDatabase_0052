5d8dc8fd5dd9a5e390052a0930820b2cf9793f45
Aditya <adityapermono8@gmail.com>
2024-12-24 03:45:58 +0700

    menambahkan LocalRepositoryJadwal untuk mengelola data Jadwal secara lokal

---
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryJadwal.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.dao.JadwalDao
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryJadwal(
+    private val jadwalDao: JadwalDao
+) : RepositoryJadwal {
+
+    override suspend fun insertJadwal(jadwal: Jadwal) {
+        jadwalDao.addJadwal(jadwal)
+    }
+
+    override fun getAllJadwal(): Flow<List<Jadwal>> {
+        return jadwalDao.getAllJadwal()
+    }
+
+    override fun getJadwal(idJadwal: String): Flow<Jadwal>{
+        return jadwalDao.getJadwal(idJadwal)
+    }
+
+    override suspend fun updateJadwal(jadwal: Jadwal) {
+        jadwalDao.updateJadwal(jadwal)
+    }
+
+    override suspend fun deleteJadwal(jadwal: Jadwal) {
+        jadwalDao.deleteJadwal(jadwal)
+    }
+
+}
